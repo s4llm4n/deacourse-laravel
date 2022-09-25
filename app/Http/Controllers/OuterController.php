e71fcde008498c6334ea8ef2d53f0abea81c155a
s4llm4n <s4llm4n@gmail.com>
2022-09-25 21:14:45 +0700

    first commit

---
+++ app/Http/Controllers/OuterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OuterController extends Controller
+{
+    public function index() 
+    {
+        echo "anjing galak";
+    }
+}
